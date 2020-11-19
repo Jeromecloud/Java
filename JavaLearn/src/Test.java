5ecbf61ddfc79970ad358ec2e59b0c627774384f
Jeromecloud <33776321+Jeromecloud@users.noreply.github.com>
2020-11-19 10:49:34 +0800

    change

--- JavaLearn/src/Test.java
+++ JavaLearn/src/Test.java
@@ -1,66 +1,8 @@
-//import java.util.Scanner;
-//public class Test{
-//	public static void main(String[] args) {
-//		StringBuffer course = new StringBuffer();
-//		System.out.println("please enter something:");
-//		Scanner sc = new Scanner(System.in);
-//		
-//		String input;
-//		while(!(input=sc.next()).equals("#")) {
-//			course.append(input);
-//			course.append("\n");
-//		}
-//		System.out.println("the course:" + course);
-//	}
-//}
+import java.sql.SQLException;
 
-//public class Test {
-//	public static void main(String[] args) {
-//		Bank tom = new Bank("Tom", 10000);
-//		Thread thread1 = new Thread();
-//		thread1.start();
-//		
-//		tom.setBalance(tom.getBalance() - 200);
-////		System.out.println(tom.getBalance());
-//	}
-//}
-//
-//class Bank {
-//	private String account;
-//	private double balance;
-//
-//	public Bank(String name, double balance) {
-//		this.account = name;
-//		this.balance = balance;
-//	}
-//
-//	public Bank() {
-//	}
-//
-//	public void setAccount(String account) {
-//		this.account = account;
-//	}
-//
-//	public String getAccount() {
-//		return account;
-//	}
-//
-//	public double getBalance() {
-//		return balance;
-//	}
-//
-//	public void setBalance(double balance) {
-//		this.balance = balance;
-//	}
-//}
-
-public class Test {
+public class Test{
 	public static void main(String[] args) {
 		
 		
 	}
 }
-
-
-
-
--- JavaLearn/src/Test2.java
+++ JavaLearn/src/Test2.java
@@ -7,18 +7,24 @@ import java.util.Scanner;
 3、小数取2位*/
 public class Test2 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("输入一个带两位小数的数字");
-		float num = input.nextFloat();
-		int  zhengshu = (int)num;
-		int xiaoshu = Math.round((num-zhengshu)*100);
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("输入一个带两位小数的数字");
+//		float num = input.nextFloat();
+//		int  zhengshu = (int)num;
+//		int xiaoshu = Math.round((num-zhengshu)*100);
 //		float xiaoshu = num - zhengshu;//小数会出现误差
-		
-//		String[] chinese = {"角","分","壹","贰","叁","肆","伍","陆","柒","捌","玖","什","佰","仟","万","亿"};
 //		System.out.println(zhengshu+"");//将基本类型转换为字符串方法1
 //		System.out.println(String.valueOf(zhengshu));//将基本类型转换为字符串方法2
 //		System.out.println((zhengshu+"").getClass());//查看类型
 //		System.out.println(String.valueOf(zhengshu) +"   "+ String.valueOf(xiaoshu));
+		Scanner input = new Scanner(System.in);
+		System.out.println("four numbers:");
+		String num = input.next();
+		System.out.println(num.charAt(0));
+		System.out.println(num.charAt(1));
+		System.out.println(num.charAt(2));
+		System.out.println(num.charAt(3));
+
 		
 		
 		
